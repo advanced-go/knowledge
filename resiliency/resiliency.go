550ec4cf492bc15ad0bfbd3300032066e27a9bd0
advanced-go <mark.wilson.go@outlook.com>
2024-11-14 10:47:43 -0600

    Create resiliency.go

---
+++ resiliency/resiliency.go
@@ -0,0 +1,18 @@
+package resiliency
+
+const ()
+
+var (
+	resources = []string{
+		"github/advanced-go/resiliency",
+		"github/advanced-go/resiliency:caseofficer",
+		"github/advanced-go/resiliency:service",
+		"github/advanced-go/resiliency:guidance",
+		"github/advanced-go/resiliency:collaborate",
+		"github/advanced-go/resiliency:knowledge",
+		"github/advanced-go/resiliency:reason",
+		"github/advanced-go/resiliency:action",
+		"github/advanced-go/resiliency:assignment",
+		"github/advanced-go/resiliency:startup",
+	}
+)
